948fbdb59c0d16dfd812bd8b4a4d1934ecf361f4
schwanbeck <julian.schwanbeck@med.uni-goettingen.de>
2020-02-18 23:39:29 +0100

    Added the one and only Jerôme Dretzke

--- gsff/main.py
+++ gsff/main.py
@@ -8,7 +8,7 @@ Matlab-Code for the Gaussian Sum FIR Filter.
 
 https://github.com/schwanbeck/GaussianSumFIRFilter
 
-Copyright (c) 2020 Julian Schwanbeck (julian.schwanbeck@med.uni-goettingen.de) and Jerôme
+Copyright (c) 2020 Jerôme Dretzke and Julian Schwanbeck (julian.schwanbeck@med.uni-goettingen.de)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -34,7 +34,7 @@ import numpy as np
 
 class GaussianSumFIR:
     """Algorithm 1 from Pak, JM: Gaussian Sum FIR Filtering for 2D Target Tracking
-    (DOI: 10.1007/s12555-018-0938-4) with changes by Julian Schwanbeck and Jerôme
+    (DOI: 10.1007/s12555-018-0938-4) with changes by Jerôme Dretzke and Julian Schwanbeck
     """
 
     def __init__(self, delta_t, n_min=0, n_max=30, n_f=3,
